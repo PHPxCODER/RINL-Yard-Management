37362e9ac43d3865efed2d3c9801ada6fcb4792f
Subhadeep <codevizag@gmail.com>
2024-05-16 09:03:56 +0530

    Update head_tags.php

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="description" content="Rashtriya Ispat Nigam Limited(RINL) is an Excellent Choice For Businesses Who Wants To Purchase Industy Grade Steel at an Affordable Price.">
 <meta name="author" content="PHPxCODER">
-<meta property="og:url"             content="" /> <?php // URL for website (link address)?>
+<meta property="og:url"             content="https://yms.phpxcoder.in" /> <?php // URL for website (link address)?>
 <meta property="og:type"            content="website" /> <?php // type of site?>
 <meta property="og:title"           content="RINL YMS" /> <?php // title of site (title of share)?>
 <meta property="og:description"     content="RINL Yard Management System" /> <?php // description of site (text which appears when sharing)?>
