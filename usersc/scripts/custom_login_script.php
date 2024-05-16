8c1356a03ced4bf5c4ed763fb4a2353416ea0fd1
Subhadeep <codevizag@gmail.com>
2024-05-16 11:42:57 +0530

    Redirect Management Users to Store Admin Page

--- usersc/scripts/custom_login_script.php
+++ usersc/scripts/custom_login_script.php
@@ -7,7 +7,7 @@
 if(hasPerm([2],$user->data()->id)){
   Redirect::to($us_url_root.'users/admin.php');
 }elseif(hasPerm([3],$user->data()->id)){
-  Redirect::to($us_url_root.'mgmt.php');
+  Redirect::to($us_url_root.'cpanel');
 }
 else{
 Redirect::to($us_url_root.$settings->redirect_uri_after_login);
